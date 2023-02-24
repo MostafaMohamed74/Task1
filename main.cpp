7ef170e45455b85f98e282d10a8ec1611d6bda3c
Mostafa A. Tawfik <85037618+MostafaAhmed264@users.noreply.github.com>
2023-02-24 13:35:44 +0200

    Added average function

--- main.cpp
+++ main.cpp
@@ -3,19 +3,22 @@
 
 long getSum(std::vector<int> &vect)
 {
-    long sum =  0;
-    for (int num: vect)
+    long sum = 0;
+    for (int num : vect)
     {
-        sum +=num;
-
+        sum += num;
     }
     return sum;
 }
 
-
-int main ()
+long getAvg(std::vector<int> &vect)
 {
-    std::vector<int> nums {10,20,30,40,50,60,70,80,90,100};
-    std::cout << "sum is : "<< getSum(nums)<<std::endl;
+    return (getSum(vect) / vect.size());
+}
 
+int main()
+{
+    std::vector<int> nums{10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    std::cout << "sum is : " << getSum(nums) << std::endl;
+    std::cout << "avg is : " << getAvg(nums) << std::endl;
 }
